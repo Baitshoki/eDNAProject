d0cc647cdc9184bd47f594535447b9a2c17ca431
Baitshoki <66728586+Baitshoki@users.noreply.github.com>
2021-09-14 05:48:04 +0200

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using eDNAWeb.Models;
+using eDNAWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace eDNAWeb.Repository
+{
+    public class Employee : Repository<Employee>, IEmployee
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public Employee(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+    }
+}
---
+++ Manager.cs
@@ -0,0 +1,20 @@
+﻿using eDNAWeb.Models;
+using eDNAWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace eDNAWeb.Repository
+{
+    public class Manager : Repository<Manager>, IManager
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public Manager(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+    }
+}
